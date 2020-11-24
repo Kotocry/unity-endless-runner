b94ca2843d792766d1b5b92adb8e6ff733389e3b
Kotocry <chekalenko.daniel@mail.ru>
2020-11-24 08:44:58 +0100

    Final project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target = null;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int variable1; // Widoczne w Inspectorze; Dostępne dla innych klas/skryptów
+    private int variable2; // Niewidoczne w Inspectorze; Niedostępne dla innych klas/skryptów
+    [SerializeField] private int variable3; // Widoczny w Inspectorze; Niedostępny dla innych klas
+    [HideInInspector] public int variable4; //Niewidoczne w Inspectorze; Dostępne dla innych klas
+
+    [SerializeField] private float movementSpeed = 7f;
+    [SerializeField] private int linesCount = 3;
+    [SerializeField] private float horizontalStep = 10f;
+
+    private int line = 0;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && line > (-linesCount / 2))
+        {
+            // line = line - 1
+            line -= 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && line < (linesCount / 2))
+        {
+            //line = line + 1
+            line += 1;
+        }
+
+        transform.position = new Vector3(line * horizontalStep, transform.position.y, transform.position.z);
+    }
+}
